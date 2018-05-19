b5abf9fb4320ad886943494016633a1e8d6335c5
Franziska Pätzold <s0554620@htw-berlin.de>
2018-05-19 07:24:40 +0200

    enhancement for leap motion controller

---
+++ Assets/CameraScriptLM.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScriptLM : MonoBehaviour
+{
+
+    public GameObject playerCube;
+    public float delaySpeed = 0.05f;
+    private Vector3 offset;
+    private Quaternion previousRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Abstand Kamera zum Objekt
+        offset = transform.position - playerCube.transform.position;
+        //offset = new Vector3 (0.0f, 1.0f, -5.0f);
+
+        //for creating a smooth slerp
+        previousRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = playerCube.transform.position + offset;
+
+        Quaternion targetRotation = Quaternion.Slerp(previousRotation, playerCube.transform.rotation, delaySpeed);
+
+        //product of Quaternion and Vector3 is Vector3 --> Matrizenmultiplikation!
+        Vector3 targetRotated = targetRotation * offset;
+
+        transform.position = playerCube.transform.position + targetRotated;
+        transform.rotation = targetRotation;
+
+        previousRotation = transform.rotation;
+    }
+}
--- Assets/Scripts/ScriptsFor1/PlayerCube.cs
+++ Assets/Scripts/ScriptsFor1/PlayerCube.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCube : MonoBehaviour {
-
-    public float rotationSpeed = 10.0f;
+    
+    public float rotationSpeed = 100.0f;
     public float movingSpeed = 10.0f;
     private float rotation = 0.0f;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        float z = Input.GetAxis("Vertical")*Time.deltaTime*movingSpeed;
-        float x = Input.GetAxis("Horizontal")*Time.deltaTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * movingSpeed;
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime;
         float rotation = x * rotationSpeed;
         transform.Translate(0.0f, 0.0f, z);
         transform.Rotate(0.0f, rotation, 0.0f);
-		
-	}
+
+    }
 }
